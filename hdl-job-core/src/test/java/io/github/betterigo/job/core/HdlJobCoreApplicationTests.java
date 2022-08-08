6f1a8cb675d95258731cc44a7743e626a757d33d
haodonglei <betteri@163.com>
2022-08-08 14:35:37 +0800

    init

---
+++ hdl-job-core/src/test/java/io/github/betterigo/job/core/HdlJobCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.betterigo.job.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HdlJobCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
